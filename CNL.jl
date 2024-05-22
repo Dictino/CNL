39036fce0c4b3e718a77d88dfba39914f37e504d
Dictino Chaos García <dchaos@dia.uned.es>
2024-05-22 11:32:06 +0200

    Ahora sí que está corregido el bug ;)
    Pues eso una segunda corrección

--- CNL.jl
+++ CNL.jl
@@ -4,6 +4,7 @@ Pkg.activate(@__DIR__)
 if !isfile("instalado")
     println("Instalando por primera vez, va a tardar un rato...")
     Pkg.instantiate()
+    touch("instalado")
 end
 using Pluto
 Pluto.run()
